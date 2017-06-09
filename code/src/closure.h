d3c58e4e0b1d143114fdddb9da87e6234686c203
Jannick Wolters <jannick.wolters@rwth-aachen.de>
2017-06-09 10:35:03 +0200

    add non committed changes to closure.h and changes cmake-file to include cpptoml

--- code/src/closure.h
+++ code/src/closure.h
@@ -14,7 +14,7 @@ private:
     Newton* _newton;
     BasisFunctions* _basis;
     blaze::DynamicMatrix<float,blaze::rowMajor>* _phi; // stores basis functions evaluated at quadrature points
-    std::vector<blaze::DynamicMatrix<float,blaze::rowMajor>*> _hPartial; // stores partial matrices for Hessian computation
+    std::vector<blaze::DynamicMatrix<float,blaze::rowMajor>*>* _hPartial; // stores partial matrices for Hessian computation
 public:
     Closure(Problem problem);
     blaze::DynamicVector<double,blaze::rowVector> SolveClosure(blaze::DynamicVector<double,blaze::rowVector> u);
--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,4 +1,5 @@
 #include <blaze/Blaze.h>
+#include <cpptoml.h>
 
 int main(){
     return 0;
