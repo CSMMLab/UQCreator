ee699ab309c60e05930dc7d7aaf769a3d2d0cbeb
Jannick Wolters <jannick.wolters@rwth-aachen.de>
2018-01-13 17:56:12 +0100

    disabled matplotlib as plotengine

--- code/src/plotengine.cpp
+++ code/src/plotengine.cpp
@@ -1,35 +1,33 @@
 #include "plotengine.h"
 #include "gnuplotlib.h"
-#include "matplotlib.h"
+//#include "matplotlib.h"
 
-PlotEngine::PlotEngine(){
+PlotEngine::PlotEngine() {}
 
-}
+PlotEngine::~PlotEngine() {}
 
-PlotEngine::~PlotEngine(){
-
-}
-
-PlotEngine* PlotEngine::Create(std::string inputFile){
-    auto file = cpptoml::parse_file(inputFile);
-    auto general = file->get_table("plot");
-    std::string timestepping = general->get_as<std::string>("engine").value_or("");
-    if(timestepping.compare("gnuplot") == 0){
+PlotEngine *PlotEngine::Create( std::string inputFile ) {
+    auto file = cpptoml::parse_file( inputFile );
+    auto general = file->get_table( "plot" );
+    std::string timestepping = general->get_as<std::string>( "engine" ).value_or( "" );
+    if( timestepping.compare( "gnuplot" ) == 0 ) {
         return new GnuplotLib();
     }
-    else if(timestepping.compare("matplotlib") == 0){
-        return new Matplotlib();
-    }
-    else{
-        std::cerr<<"Invalid plot engine type"<<std::endl;
-        exit(EXIT_FAILURE);
+    /*
+        else if( timestepping.compare( "matplotlib" ) == 0 ) {
+            return new Matplotlib();
+        }
+    */
+    else {
+        std::cerr << "Invalid plot engine type" << std::endl;
+        exit( EXIT_FAILURE );
         return NULL;
     }
 }
 
-std::vector<double> PlotEngine::BlazeToStdVector(const blaze::DynamicVector<double>& v){
-    std::vector<double> ret(v.size(), 0.0);
-    for(unsigned i=0; i<v.size(); ++i){
+std::vector<double> PlotEngine::BlazeToStdVector( const blaze::DynamicVector<double> &v ) {
+    std::vector<double> ret( v.size(), 0.0 );
+    for( unsigned i = 0; i < v.size(); ++i ) {
         ret[i] = v[i];
     }
     return ret;
