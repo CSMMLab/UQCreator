439420b4f93b8b89f176538b8fba9625691ea660
Unknown <jonas.kusch@rwth-aachen.de>
2018-11-27 17:39:00 +0100

    back to 1D

--- code/src/settings.cpp
+++ code/src/settings.cpp
@@ -1,6 +1,6 @@
 #include "settings.h"
 
-Settings::Settings( std::string inputFile ) : _inputFile( inputFile ), _numDimXi( 2 ) {
+Settings::Settings( std::string inputFile ) : _inputFile( inputFile ), _numDimXi( 1 ) {
     auto log = spdlog::get( "event" );
 
     bool validConfig = true;
