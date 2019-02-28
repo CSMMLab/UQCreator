8d3a2c032b6103ebb4b9ece3985fce89f0188048
Jannick Wolters <jannick.wolters@rwth-aachen.de>
2019-02-28 17:57:39 +0100

    convergence plot script is now working with the current code

--- scripts/convergence_plot/plot.py
+++ scripts/convergence_plot/plot.py
@@ -2,65 +2,124 @@ import matplotlib.pyplot as plt
 import argparse
 import numpy as np
 import datetime as dt
-import os, time
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--file", "-f", type=str, required=True)
-parser.add_argument("--compareTo", "-c", type=str, required=False)
-parser.add_argument("--live", "-l", action='store_true', required=False)
-args = parser.parse_args()
-
-def splitLine(line, splitchar):
-    list = line.split(splitchar)
-    list = [" ".join(x.split()) for x in list]
-    return list
+import os
+import pandas as pd
+from io import StringIO
 
 def time_to_float(time):
     t = dt.datetime.strptime(time, "%Y-%m-%d %H:%M:%S.%f")
     epoch = dt.datetime.utcfromtimestamp(0)
     return (t-epoch).total_seconds()
 
-def plot(x,y,xlabel,ylabel):
+def create_label(dir, file):    
+    section_ctr = 0
+    line_ctr = 0    
+    with open(dir+'/'+file, 'r') as content:
+        lines = content.readlines()        
+        closure = ''
+        nQuad = -1
+        nMoments = -1   
+        maxIter = -1
+        restartFile = False
+        while section_ctr<3:            
+            if "#" in lines[line_ctr]:
+                line_ctr = line_ctr + 1
+                continue
+            if "=====" in lines[line_ctr]:
+                section_ctr = section_ctr + 1
+            if "closure" in lines[line_ctr]:
+                closure = lines[line_ctr].split("=")[1].strip().strip('"').strip('\n')
+            elif "moments" in lines[line_ctr]:
+                nQuad = int(lines[line_ctr].split("=")[1])
+            elif "quadPoints" in lines[line_ctr]:
+                nMoments = int(lines[line_ctr].split("=")[1])
+            elif "maxIterations" in lines[line_ctr]:
+                maxIter = int(lines[line_ctr].split("=")[1])
+            elif "restartFile" in lines[line_ctr] and not "#restartFile" in lines[line_ctr]:
+                restartFile = True
+            line_ctr = line_ctr + 1        
+        if closure == 'StochasticGalerkin' and nQuad == nMoments:
+            return 'SC'
+        elif closure == 'StochasticGalerkin':
+            return 'SG'
+        elif closure == 'Euler2D' and restartFile:
+            return 'caos-IPM'
+        elif closure == 'Euler2D' and maxIter == 1:
+            return 'os-IPM'
+        elif closure == 'Euler2D':
+            return 'IPM'
+        else:
+            return 'unkown'
+
+def parse_logfile(dir, file):
+    section_ctr = 0
+    line_ctr = 0
+    with open(dir+'/'+file, 'r') as content:
+        lines = content.readlines()        
+        while section_ctr<3:
+            if "=====" in lines[line_ctr]:
+                section_ctr = section_ctr + 1
+            line_ctr = line_ctr + 1
+        line_ctr = line_ctr + 1
+        while "PE" in lines[line_ctr] or "residual" in lines[line_ctr]:
+            line_ctr = line_ctr + 1
+        lines = lines[line_ctr:]
+        lines_to_del = []
+        for i in range(len(lines)):
+            if any(x in lines[i] for x in ['a','i','o','u']): #sketchy but works
+                lines_to_del.append(i)
+        for line in reversed(lines_to_del):
+            del lines[line]
+        header = ['date', 'time', 'delim', 't', 'residual_scaled', 'residual_abs']
+        df = pd.read_csv(StringIO("\n".join(lines)), header=None, names=header, delim_whitespace = True)
+        df = df.drop(columns=['delim'])
+        df = df.dropna()
+        t0 = time_to_float(df['date'][0]+' '+df['time'][0])
+        df['runtime'] = np.zeros(len(df['time']))
+        for index, row in df.iterrows():
+            df.loc[index,'runtime'] = time_to_float(df['date'][index]+' '+df['time'][index]) - t0
+        df = df.drop(columns=['date','time'])
+        return(df)
+
+def create_runtime_residual_plots(data, labels, threshold=np.inf):   
     plt.cla()
     plt.clf()
-    plt.plot(x,y)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.savefig(xlabel+"_"+ylabel+".pdf")
+    for df in data:
+        df = df[df['residual_scaled'] < threshold]
+        plt.semilogy(df['runtime'],df['residual_scaled'])
+    plt.xlabel('Runtime [s]')
+    plt.ylabel('Residual')
+    plt.legend(labels)
+    plt.savefig("convergence_runtime_residual.pdf")
 
-def create_plots():
-    t_arr = []
-    residual_arr = []
-    iter_arr = []
-    timestamp_arr = []
-    with open(args.file, 'r') as f:
-        for line in f:
-            [timestamp, data] = splitLine(line, '|')
-            data_list = splitLine(data, ' ')
-            try:
-                for d in data_list:
-                    d = float(d)
-            except ValueError:
-                continue
-            data_list = [float(x) for x in data_list]
-            t_arr.append(data_list[0])
-            residual_arr.append(data_list[1])
-            timestamp_arr.append(time_to_float(timestamp))
-    for t in timestamp_arr:
-        t = t-timestamp_arr[0]
-    iter_arr = np.linspace(1, len(t_arr), len(t_arr))
-    plot(iter_arr, residual_arr, 'iteration', 'residual')
-    plot(iter_arr, timestamp_arr, 'iteration', 'walltime')
-    plot(timestamp_arr, residual_arr, 'walltime', 'residual')
-    plot(timestamp_arr, t_arr, 'walltime', 'time')
+def create_time_residual_plots(data, labels, threshold=np.inf):   
+    plt.cla()
+    plt.clf()
+    for df in data:
+        df = df[df['residual_scaled'] < threshold]
+        plt.semilogy(df['t'],df['residual_scaled'])
+    plt.xlabel('Time [s]')
+    plt.ylabel('Residual')
+    plt.legend(labels)
+    plt.savefig("convergence_time_residual.pdf")
 
-
-if(args.live):
-    last_modified = os.stat(args.file).st_mtime
-    while True:
-        time.sleep(10)
-        if(last_modified != os.stat(args.file).st_mtime):
-            create_plots()
-            last_modified = os.stat(args.file).st_mtime
-else:
-    create_plots()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dir", "-d", type=str, required=True)
+    parser.add_argument("--threshold", "-t", type=str, required=False)
+    args = parser.parse_args()
+    data = []
+    labels = []
+    for file in os.listdir(args.dir):
+        if file.endswith('_moments') or file.endswith('_duals'):
+            print("Skipping '" + file + "'!")
+            continue
+        print("Processing '" + file + "'...")
+        data.append(parse_logfile(args.dir, file))
+        labels.append(create_label(args.dir, file))
+    if(args.threshold):
+        create_runtime_residual_plots(data, labels, args.threshold)
+        create_time_residual_plots(data, labels, args.threshold)
+    else:
+        create_runtime_residual_plots(data, labels)
+        create_time_residual_plots(data, labels)
