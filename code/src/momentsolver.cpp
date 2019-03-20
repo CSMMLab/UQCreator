374dc6123d0a06a1fa6c0b9fb3798975c840cfe4
Jannick Wolters <jannick.wolters@kit.edu>
2019-03-20 20:06:55 +0100

    changed back to l1 residual

--- code/src/momentsolver.cpp
+++ code/src/momentsolver.cpp
@@ -67,7 +67,7 @@ void MomentSolver::Solve() {
     double residualFull = minResidual + 1.0;
 
     // Begin time loop
-    while( t < _tEnd && std::sqrt( residualFull ) > minResidual ) {
+    while( t < _tEnd && residualFull > minResidual ) {
         double residual = 0;
 
 #pragma omp parallel for schedule( dynamic, 10 )
@@ -111,7 +111,7 @@ void MomentSolver::Solve() {
         }
         MPI_Allreduce( &residual, &residualFull, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD );
         if( _settings->GetMyPE() == 0 ) {
-            log->info( "{:03.8f}   {:01.5e}   {:01.5e}", t, std::sqrt( residualFull ), residualFull / dt );
+            log->info( "{:03.8f}   {:01.5e}   {:01.5e}", t, residualFull, residualFull / dt );
         }
     }
 
