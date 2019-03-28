d4846744271d34154b61426d1d6e07b378734202
Jannick Wolters <jannick.wolters@kit.edu>
2019-03-28 15:10:59 +0100

    overwriting OMP_NUM_THREADS is not a good idea -> removed -t argument

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -19,7 +19,6 @@ bool CheckInput( std::string& configFile, int argc, char* argv[] ) {
                              std::string( argv[0] ) +
                              " -c inputfile\n\n"
                              "Options:\n"
-                             "  -t N             number of threads to use\n"
                              "  -h               displays this message\n";
 
     if( argc < 3 ) {
@@ -39,9 +38,6 @@ bool CheckInput( std::string& configFile, int argc, char* argv[] ) {
                 std::cerr << "[ERROR] Unable to open specified inputfile!" << std::endl;
                 return false;
             }
-        }
-        else if( arg == "-t" ) {
-            omp_set_num_threads( std::stoi( argv[++i] ) );
         }
         else {
             std::cout << usage_help;
