7166c31fa812b6b34dec7863e537fd6e7e541b49
Jannick Wolters <jannick.wolters@kit.edu>
2019-08-20 08:59:38 +0200

    undid change

--- code/src/settings.cpp
+++ code/src/settings.cpp
@@ -119,9 +119,6 @@ void Settings::Init( std::shared_ptr<cpptoml::table> file, bool restart ) {
                 _referenceFile = _inputDir.string() + "/" + *refFile;
             }
         }
-        else {
-            _writeFrequency = general->get_as<unsigned>( "writeFrequency" ).value_or( 100 );
-        }
 
         // section problem
         auto problem                = file->get_table( "problem" );
