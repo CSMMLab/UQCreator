a6842c267f249e89446e158a09c33d54e18674ab
Unknown <jonas.kusch@rwth-aachen.de>
2019-08-22 10:49:00 +0200

    new pipe IC

--- code/src/euler2d.cpp
+++ code/src/euler2d.cpp
@@ -161,9 +161,50 @@ double Euler2D::ComputeDt( const Matrix& u, double dx, unsigned level ) const {
 
 Vector Euler2D::IC( const Vector& x, const Vector& xi ) {
     Vector y( _nStates );
-    _sigma            = _settings->GetSigma();
-    bool pipeTestCase = false;
-    if( pipeTestCase ) {    // pipe testcase
+    _sigma              = _settings->GetSigma();
+    bool pipeTestCase   = false;
+    bool pipeTestCaseMC = false;
+    if( pipeTestCaseMC ) {
+        double gamma = 1.4;
+        double R     = 287.87;
+        double T     = 273.15;
+        double p     = 101325.0;
+        double Ma    = 0.0;
+        double a     = sqrt( gamma * R * T );
+
+        double uMax  = Ma * a;
+        double angle = ( 1.25 + _sigma[0] * 0.0 ) * ( 2.0 * M_PI ) / 360.0;
+        double uF    = uMax * cos( angle );
+        double vF    = uMax * sin( angle );
+
+        double rhoFarfield = p / ( R * T );
+
+        y[0]                  = rhoFarfield;
+        y[1]                  = rhoFarfield * uF;
+        y[2]                  = rhoFarfield * vF;
+        double kineticEnergyL = 0.5 * rhoFarfield * ( pow( uF, 2 ) + pow( vF, 2 ) );
+        double innerEnergyL   = ( p / ( rhoFarfield * ( gamma - 1 ) ) ) * rhoFarfield;
+        y[3]                  = kineticEnergyL + innerEnergyL;
+
+        if( x[1] < 1.1 + _sigma[0] * xi[0] ) {
+            y[0] = 0.5 * rhoFarfield;
+            y[1] = rhoFarfield * uF;
+            y[2] = rhoFarfield * vF;
+            y[3] = 0.5 * ( kineticEnergyL + innerEnergyL );
+        }
+        /*
+         if( x[1] < 1.1 + sigma && x[1] < 1.1 - sigma ) {
+             y[0] = 0;
+         }
+         if( x[1] < 1.1 + sigma && x[1] > 1.1 - sigma ) {
+             y[0] = 1;
+         }
+         if( x[1] > 1.1 + sigma && x[1] > 1.1 - sigma ) {
+             y[0] = 2;
+         }*/
+        return y;
+    }
+    else if( pipeTestCase ) {    // pipe testcase
         double gamma = 1.4;
         double R     = 287.87;
         double T     = 273.15;
